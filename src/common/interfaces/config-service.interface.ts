b55d85a3f4632355676ed5b73ade02449849d6a8
MahdiProgman <mahdihabibkah78@gmail.com>
2025-10-22 17:20:33 +0330

    removed common folder

--- src/common/interfaces/config-service.interface.ts
+++
@@ -1,6 +0,0 @@
-import { AppConfig, Configuration } from 'src/config/config.interface';
-
-export interface IConfigService {
-  getEntireConfiguration: () => Configuration;
-  getAppConfig: () => AppConfig;
-}
