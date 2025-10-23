0b3e78a9dfcf4e48ca8f8780b241388de3f3025b
MahdiProgman <mahdihabibkah78@gmail.com>
2025-10-23 12:19:47 +0330

    installed @nestjs/swagger package and configured swagger on project

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/contracts/config.service';
 import { CONFIG_SERVICE } from './config/constants';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(CONFIG_SERVICE);
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager API')
+    .setDescription('Documentation for best of the best')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
 
   const port = configService.getAppConfig().port;
 
