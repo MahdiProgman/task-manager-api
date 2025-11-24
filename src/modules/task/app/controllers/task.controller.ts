a81ec64b115ae3c83a6fe2e270d84c95a23267ea
MahdiProgman <mahdihabibkah78@gmail.com>
2025-11-24 22:11:01 +0330

    docs: writed docs for create new task endpoint

--- src/modules/task/app/controllers/task.controller.ts
+++ src/modules/task/app/controllers/task.controller.ts
@@ -3,9 +3,16 @@ import { TaskService } from '../services/task.service';
 import { Request } from 'express';
 import { SuccessResponse } from 'src/common/types';
 import { AuthGuard } from 'src/common/guards/auth.guard';
-import { ApiOkResponse } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+} from '@nestjs/swagger';
 import { GetAllTasksResponse } from './responses/get-all-tasks-response.dto';
 import { CreateTaskDto } from '../dtos/create-task.dto';
+import { buildFailedResponse } from 'src/common/tools/swagger';
+import { CreateTaskResponse } from './responses/create-task.dto';
+import { CategoryNotFoundError } from '../exceptions/category-not-found.exception';
 
 @UseGuards(AuthGuard)
 @Controller('tasks')
@@ -23,6 +30,12 @@ export class TaskController {
     };
   }
 
+  @ApiCreatedResponse({
+    type: CreateTaskResponse,
+  })
+  @ApiNotFoundResponse({
+    example: buildFailedResponse(new CategoryNotFoundError()),
+  })
   @Post('new')
   public async createNewTask(
     @Req() req: Request,
