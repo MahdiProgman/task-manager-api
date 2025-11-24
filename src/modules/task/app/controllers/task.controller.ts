62e023308f9d897ba00d1eacf0e59eaa44321d89
MahdiProgman <mahdihabibkah78@gmail.com>
2025-11-24 15:51:44 +0330

    docs: writed docs for get all tasks endpoint

--- src/modules/task/app/controllers/task.controller.ts
+++ src/modules/task/app/controllers/task.controller.ts
@@ -3,12 +3,15 @@ import { TaskService } from '../services/task.service';
 import { Request } from 'express';
 import { SuccessResponse } from 'src/common/types';
 import { AuthGuard } from 'src/common/guards/auth.guard';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { GetAllTasksResponse } from './responses/get-all-tasks-response.dto';
 
 @UseGuards(AuthGuard)
 @Controller('tasks')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
+  @ApiOkResponse({ type: [GetAllTasksResponse] })
   @Get()
   public async getAllTasks(@Req() req: Request): Promise<SuccessResponse> {
     const tasks = await this.taskService.getAllTasks(req.userId);
