f48bfcd5f0158c91f2f3ad402acab350660fb426
MahdiProgman <mahdihabibkah78@gmail.com>
2025-11-24 20:07:40 +0330

    created create new task endpoint

--- src/modules/task/app/controllers/task.controller.ts
+++ src/modules/task/app/controllers/task.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { TaskService } from '../services/task.service';
 import { Request } from 'express';
 import { SuccessResponse } from 'src/common/types';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { GetAllTasksResponse } from './responses/get-all-tasks-response.dto';
+import { CreateTaskDto } from '../dtos/create-task.dto';
 
 @UseGuards(AuthGuard)
 @Controller('tasks')
@@ -19,6 +20,19 @@ export class TaskController {
     return {
       statusCode: 200,
       data: tasks,
+    };
+  }
+
+  @Post('new')
+  public async createNewTask(
+    @Req() req: Request,
+    @Body() dto: CreateTaskDto,
+  ): Promise<SuccessResponse> {
+    const newTask = await this.taskService.createNewTask(req.userId, dto);
+
+    return {
+      statusCode: 201,
+      data: newTask,
     };
   }
 }
